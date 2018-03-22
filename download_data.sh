03cabff64af9f12cb8ff402569fa5042f3a3111f
marcinkawka <kawka.marcin@gmail.com>
2018-03-22 13:21:48 +0100

    Added download and basic plotting Kamienna

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-wget -e robots=off --cut-dirs=3 --user-agent=Mozilla/5.0 --reject="index.html*" --no-parent --recursive --relative --level=1 --no-directories https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/2016/
+base_url="https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/2016/"
+prefix="wget -e robots=off --cut-dirs=3 --user-agent=Mozilla/5.0 --reject="index.html*" --no-parent --recursive --relative --level=1 --no-directories"
+mkdir incoming
+cd incoming
+
+ #`$prefix $base_url`
+for f in *.zip
+do
+	unzip $f
+done
 
--- load_data.py
+++ load_data.py
@@ -1,11 +1,48 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
 
-df = pd.read_csv('codz_2016_01.csv')
 
-df = pd.read_csv('incoming/codz_2016_01.csv',encoding='windows-1250',header=None)
+
+df = pd.read_csv('incoming/codz_2016_01.csv',encoding='windows-1250',header=None,na_values=[99999.99900000001,99.9],na_filter=True,verbose=True)
 
 kam = df[df[2]=='Kamienna (234)']
 
-kam_dat = pd.Timestamp(datetime(kam[3],kam[9],kam[5]))
+dd=pd.to_datetime(kam[3].astype(str)+kam[9].astype(str)+kam[5].astype(str),format='%Y%m%d')
+
+discharge_gauges = kam[1].unique()
+
+prefix = 'incoming/codz_2016_'
+dd={}
+for dg in discharge_gauges:
+
+	daty=pd.Series()
+	Q=pd.Series()
+	H=pd.Series()
+
+	for mon in range(1,13):
+		file = prefix+str(mon).zfill(2)+'.csv'
+		df = pd.read_csv(file,encoding='windows-1250',header=None)
+		kam = df[df[1]==dg][df[2]=='Kamienna (234)']
+
+		if(mon<3):	#przesuniecie roku z powodu roku hydrologicznego
+			shift = 1	
+		else:
+			shift = 0
+		daty = daty.append(pd.to_datetime((kam[3]-shift).astype(str)+kam[9].astype(str)+kam[5].astype(str),format='%Y%m%d'))
+		H=H.append(kam[6])
+		Q=Q.append(kam[7])
+
+	ts=pd.Series(np.array(Q),index=daty)
+	dd[dg]=ts
+#ts=ts.cumsum()
+
+#	ts.plot()
+#	plt.show()
+
+df = pd.DataFrame(data=dd)
+df=df.replace({99999.99900000001:np.nan})
+
+df.plot()
+plt.savefig('asdf.pdf',papertype='a3')
